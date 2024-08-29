b270cc4d65d7b743832dea4c69986fa1b4c03bd0
Kshitij Gunjalkar <42808290+gunzalkar@users.noreply.github.com>
2024-08-30 02:16:42 +0530

    Update huawei_routers.py

--- huawei_routers.py
+++ huawei_routers.py
@@ -36,7 +36,7 @@ CHECKS = [
         'expected_output': ['Telnet', 'SSH', 'HTTP', 'SNMP', 'FTP', 'ICMP']
     },
     {
-        'objective': 'Management Pane MPAC Configuration',
+        'objective': 'Management Plane MPAC Configuration',
         'commands': [
             'system-view',
             'service-security policy ipv4 test',
@@ -83,6 +83,53 @@ CHECKS = [
             'security wpa-wpa2 dot1x aes-tkip'
         ],
         'expected_output': ['WPA-WPA2-PSK', 'WPA-WPA2-802.1X', 'TKIP-AES']
+    },
+    {
+        'objective': 'Forwarding Plane ACL',
+        'commands': [
+            'system-view',
+            'acl 2001',
+            'rule permit source 192.168.32.1 0'
+        ],
+        'expected_output': ['ACL 2001', 'rule permit source 192.168.32.1 0']
+    },
+    {
+        'objective': 'Forwarding Plane Traffic Suppression and Storm Control',
+        'commands': [
+            'system-view',
+            'interface gigabitethernet 1/0/1',
+            'broadcast-suppression 30',
+            'multicast-suppression 30',
+            'unicast-suppression 30',
+            'quit'
+        ],
+        'expected_output': ['broadcast-suppression 30', 'multicast-suppression 30', 'unicast-suppression 30']
+    },
+    {
+        'objective': 'Forwarding Plane Trusted Path-based Forwarding',
+        'commands': [
+            'system-view',
+            'urpf strict'
+        ],
+        'expected_output': ['URPF strict mode']
+    },
+    {
+        'objective': 'Management Plane Information Center Security',
+        'commands': [
+            'system-view',
+            'ssl policy Example@123',
+            'info-center loghost 192.168.2.2 transport tcp ssl-policy Example@123'
+        ],
+        'expected_output': ['info-center loghost 192.168.2.2', 'transport tcp ssl-policy Example@123']
+    },
+    {
+        'objective': 'Management Plane HWTACACS User Management Security',
+        'commands': [
+            'system-view',
+            'hwtacacs-server template test1',
+            'hwtacacs-server shared-key cipher YsHsjx_202206'
+        ],
+        'expected_output': ['hwtacacs-server shared-key cipher YsHsjx_202206']
     }
 ]
 
@@ -107,8 +154,9 @@ def validate_output(output, expected):
 def execute_check(shell, check):
     if 'commands' in check:
         # Execute each command in the list
+        output = ''
         for command in check['commands']:
-            output = run_command(shell, command)
+            output += run_command(shell, command)
     else:
         # Single command case
         output = run_command(shell, check['command'])
