207c0bd9c269071e9f76ed2d4da47b4502e2ff5d
Kshitij Gunjalkar <42808290+gunzalkar@users.noreply.github.com>
2024-09-13 13:46:41 +0530

    Update hp.py

--- hp.py
+++ hp.py
@@ -57,7 +57,7 @@ def check_secure_management_vlan_compliance(shell):
 # MBSS 7 - Authorized IP Managers Check
 def check_authorized_ip_managers_compliance(shell):
     output = run_command(shell, 'display acl 200')
-    return 'rule 10' and 'rule 20' in output
+    return 'rule 10' in output
 
 results = []
 
